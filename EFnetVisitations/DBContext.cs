fccf20fe5c5d8d3e5e6dd56ed7f25379be2f7606
FreonZ32 <tolyagenii32@gmail.com>
2023-02-22 23:46:31 +0500

    HW

---
+++ EFnetVisitations/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFnetVisitations
+{
+    internal class DBContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=hello.db";
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+
+        public DbSet<Student> Students => Set<Student>();
+    }
+}
---
+++ EFnetVisitations/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EFnetVisitations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await using var db = new DBContext();
+            await db.Database.EnsureCreatedAsync();
+            await db.Students.AddAsync(new Student() { Name = "Adam", Birthday = DateTime.Now });
+            await db.Students.AddAsync(new Student() { Name = "Saver", Birthday = DateTime.Today.AddDays(-2) });
+            await db.Students.AddAsync(new Student() { Name = "Gagga", Birthday =  DateTime.Today.AddDays(-1)});
+            await db.SaveChangesAsync();
+            var students = await db.Students.ToListAsync();
+            studentsDG.ItemsSource = students;
+        }
+    }
+}
