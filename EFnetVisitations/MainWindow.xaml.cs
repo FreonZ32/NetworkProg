892e8014b9abdbadb1968e593672873502b1dfa4
Anatoliy Chemodanov <freonstalteh@gmail.com>
2023-04-11 11:29:15 +0500

    HW

--- EFnetVisitations/MainWindow.xaml.cs
+++ EFnetVisitations/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace EFnetVisitations
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow: Window
     {
         private DBContext _db = new DBContext();
         List<Student> studentList;
